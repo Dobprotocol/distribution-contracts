d50a88e5581c4d45a056119a64920aa5df98ac27
Francisco <francisco.munoz@forcast.cl>
2024-12-23 16:52:10 -0300

    🎨 improve code and documentation

--- contracts/contract/staking/SimpleLockedStaking.sol
+++ contracts/contract/staking/SimpleLockedStaking.sol
@@ -8,7 +8,13 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     using SafeERC20 for ERC20;
 
-    // struct used to facilitate handling of staking configuration
+    /**
+     * @dev Struct to define the staking configuration parameters.
+     * @param rewardRate Annualized reward rate (percentage, multiplied by 10000 for precision).
+     * @param lockDays Duration (in days) for which the tokens will be locked.
+     * @param depositDays Duration (in days) during which users can deposit tokens.
+     * @param startDate Timestamp when the staking period starts.
+     */
     struct StakingConfig {
         uint256 rewardRate;
         uint256 lockDays;
@@ -16,7 +22,13 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
         uint256 startDate;
     }
 
-    // enumeration used to facilitate handling of states
+    /**
+     * @dev Enum to define the different states of the staking configuration.
+     * - NotSet: Configuration has not been initialized.
+     * - Opened: Staking is open for deposits.
+     * - Locked: Staking is in the locking phase (deposits closed, tokens locked).
+     * - Completed: Staking period is completed (users can claim their rewards).
+     */
     enum ConfigState {
         Opened,
         Locked,
@@ -24,38 +36,65 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
         NotSet
     }
 
-    // erc20 tokens
-    ERC20 private immutable _stakeToken;
-    ERC20 private immutable _rewardToken;
+    // State variables
+    ERC20 private immutable _stakeToken; // ERC20 token used for staking
+    ERC20 private immutable _rewardToken; // ERC20 token used for rewards
+    uint256 private _activeUsersCounter; // Number of active users with staked tokens
+    uint256 private _totalStaked; // Total staked tokens
+    uint256 private _claimedRewards; // Total claimed rewards
+    mapping(address => uint256) private _stakedPerUser; // Mapping of user addresses to staked amounts
 
-    // usage variables
-    uint256 _activeUsersCounter;
-    uint256 _totalStaked;
-    uint256 _claimedRewards;
-    mapping(address => uint256) _stakedPerUser;
-
-    // config
+    // Staking configuration
     StakingConfig private C;
-    uint256 private constant REWARD_FACTOR = 10000;
-    uint256 private constant DAY_TO_SECONDS = 86400;
+    uint256 private constant REWARD_FACTOR = 10000; // Factor for reward rate precision
+    uint256 private constant DAY_TO_SECONDS = 86400; // Number of seconds in a day
 
     //////////////////////////////////
     // events
     //////////////////////////////////
+
+    /**
+     * @dev Emitted when the staking configuration is set.
+     * @param rewardRateOver10k Reward rate (percentage, multiplied by 10000).
+     * @param lockPeriodDuration Duration of the lock period (in days).
+     * @param depositPeriodDuration Duration of the deposit period (in days).
+     * @param startDate Start date of the staking period (timestamp).
+     */
     event ConfigSet(
         uint256 rewardRateOver10k,
         uint256 lockPeriodDuration,
         uint256 depositPeriodDuration,
         uint256 startDate
     );
+
+    /**
+     * @dev Emitted when a user stakes tokens.
+     * @param user Address of the user who staked tokens.
+     * @param amount Amount of tokens staked.
+     */
     event StakeTokens(address user, uint256 amount);
+
+    /**
+     * @dev Emitted when a user claims their staked tokens and rewards.
+     * @param user Address of the user claiming tokens.
+     * @param amountStake Amount of staked tokens claimed.
+     * @param amountReward Amount of reward tokens claimed.
+     */
     event ClaimTokens(address user, uint256 amountStake, uint256 amountReward);
+
+    /**
+     * @dev Emitted when the contract owner withdraws remaining reward tokens.
+     * @param amount Amount of reward tokens withdrawn.
+     */
     event WithdrawRemains(uint256 amount);
 
     //////////////////////////////////
     // modifiers
     //////////////////////////////////
 
+    /**
+     * @dev Ensures the staking configuration is in the "Completed" state.
+     */
     modifier isCompleted() {
         require(
             getState() == ConfigState.Completed,
@@ -64,6 +103,9 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
         _;
     }
 
+    /**
+     * @dev Ensures the staking configuration is in the "Opened" state.
+     */
     modifier isOpened() {
         require(
             getState() == ConfigState.Opened,
@@ -72,6 +114,9 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
         _;
     }
 
+    /**
+     * @dev Ensures the staking configuration is in the "NotSet" state.
+     */
     modifier isNotSet() {
         require(
             getState() == ConfigState.NotSet,
@@ -80,6 +125,15 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
         _;
     }
 
+    //////////////////////////////////
+    // Constructor
+    //////////////////////////////////
+
+    /**
+     * @dev Initializes the contract with staking and reward tokens.
+     * @param stakingToken ERC20 token used for staking.
+     * @param rewardsToken ERC20 token used for rewards.
+     */
     constructor(ERC20 stakingToken, ERC20 rewardsToken) {
         require(
             address(stakingToken) != address(rewardsToken),
@@ -90,27 +144,13 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     }
 
     //////////////////////////////////
-    // internal functions
-    //////////////////////////////////
-
-    function _safeTransfer(ERC20 token, address to, uint256 amount) internal {
-        // do i really need this require() if using safeTransfer()???
-        require(
-            token.balanceOf(address(this)) >= amount,
-            "not enough tokens to transfer"
-        );
-        token.safeTransfer(to, amount);
-    }
-
-    //////////////////////////////////
     // deposit/withdraw functions
     //////////////////////////////////
 
     /**
-     * Stake the specified amount and link it to the msg.user
-     * This function can only be used when the stake is in state Opened
-     *
-     * @param _amount the amount to stake
+     * @notice Allows a user to stake tokens during the deposit period.
+     * @dev Ensures the staking configuration is in the "Opened" state.
+     * @param _amount Amount of tokens to stake.
      */
     function stake(uint256 _amount) external nonReentrant isOpened {
         uint256 maxStake = estimateStake(getRewardTokenBalance());
@@ -121,15 +161,17 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
         _stakeToken.safeTransferFrom(msg.sender, address(this), _amount);
 
         // update variables
+        if (_stakedPerUser[msg.sender] == 0) {
+            _activeUsersCounter++;
+        }
         _stakedPerUser[msg.sender] += _amount;
         _totalStaked += _amount;
-        _activeUsersCounter++;
         emit StakeTokens(msg.sender, _amount);
     }
 
     /**
-     * Claim the staked and reward tokens for the msg.user
-     * This function can only be used when the stake is in state Completed
+     * @notice Allows a user to claim their staked tokens and rewards after the lock period.
+     * @dev Ensures the staking configuration is in the "Completed" state.
      */
     function claim() external nonReentrant isCompleted {
         uint256 stakedAmount = getUserStakedAmount(msg.sender);
@@ -141,8 +183,8 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
         );
 
         // transfer the tokens
-        _safeTransfer(_stakeToken, msg.sender, stakedAmount);
-        _safeTransfer(_rewardToken, msg.sender, expectedReward);
+        _stakeToken.safeTransfer(msg.sender, stakedAmount);
+        _rewardToken.safeTransfer(msg.sender, expectedReward);
 
         // make the variables updates
         _stakedPerUser[msg.sender] = 0;
@@ -153,15 +195,14 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     }
 
     /**
-     * withdraw any non-locked reward token
-     *
-     * The withdrawal can only be executed when the stake is on state Completed.
+     * @notice withdraw any non-locked reward token
+     * @dev Ensures the staking configuration is in the "Completed" state.
      */
     function withdrawRemains() external onlyOwner nonReentrant isCompleted {
+        require(getRewardTokenBalance() > getTotalLockedRewards(), "no tokens available to withdraw");
         uint256 withdrawTokens = getRewardTokenBalance() -
             getTotalLockedRewards();
-        require(withdrawTokens > 0, "no tokens available to withdraw");
-        _safeTransfer(_rewardToken, msg.sender, withdrawTokens);
+        _rewardToken.safeTransfer(msg.sender, withdrawTokens);
         emit WithdrawRemains(withdrawTokens);
     }
 
@@ -170,9 +211,9 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     //////////////////////////////////
 
     /**
-     * set the staking config.
-     * This function can only be called once, since after the configuration is set, the state
-     * will no longer be NotSet.
+     * @notice set the staking config.
+     * @dev This function can only be called once, since after the configuration is set, 
+     * the state will no longer be NotSet.
      *
      * @param config the configuration to set
      */
@@ -202,7 +243,7 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     }
 
     /**
-     * returns the configuration parameters used for the staking.
+     * @notice returns the configuration parameters used for the staking.
      */
     function getConfig() external view returns (StakingConfig memory) {
         return C;
@@ -213,7 +254,7 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     //////////////////////////////////
 
     /**
-     * returns the current configuration usage data
+     * @notice gets the current configuration usage data
      *
      * @return activeUsersCount the number distinct unique address that have staked tokens
      * @return totalStaked the total amount of staked tokens
@@ -237,45 +278,50 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     }
 
     /**
-     * @param user gets the total staked amount for this user
+     * @notice gets the total staked tokens for the given user
+     * @param user the user to get its total staked tokens
      */
     function getUserStakedAmount(address user) public view returns (uint256) {
         return _stakedPerUser[user];
     }
 
     /**
-     * returns the total locked reward tokens given the configuration of stake used.
+     * @notice returns the total locked reward tokens given the configuration of stake used.
      * The locked amount vary depending on which state the config currently is.
      *
-     * If the state is Locked or Completed, the locked reward tokens will be the estimated
-     * reward for the currently total staked tokens.
-     *
-     * If the config is in any other state, then the whole balance is locked.
+     * @dev If the state is Locked or Completed, the locked reward tokens will be the estimated
+     * reward for the currently total staked tokens. If the config is in any other state, 
+     * then the whole balance is locked.
      */
     function getTotalLockedRewards() public view returns (uint256) {
-        ConfigState state = getState();
-        if (state == ConfigState.Locked || state == ConfigState.Completed) {
+        if (
+            getState() == ConfigState.Locked ||
+            getState() == ConfigState.Completed
+        ) {
             return estimateRewards(_totalStaked);
         }
         return getRewardTokenBalance();
     }
 
     /**
-     * gets the staking state based on the block timestamp and the
+     * @notice gets the staking state based on the block timestamp and the
      * given configuration.
      */
     function getState() public view returns (ConfigState) {
+        if (C.startDate == 0) return ConfigState.NotSet;
+
         uint256 ts_ = block.timestamp;
         uint256 startLock = C.startDate + C.depositDays;
-        if (C.startDate == 0) return ConfigState.NotSet;
+
         if (C.startDate <= ts_ && ts_ < startLock) return ConfigState.Opened;
-        else if (startLock <= ts_ && ts_ < startLock + C.lockDays)
+        if (startLock <= ts_ && ts_ < startLock + C.lockDays)
             return ConfigState.Locked;
-        else return ConfigState.Completed;
+
+        return ConfigState.Completed;
     }
 
     /**
-     * estimate the rewards for the specified user
+     * @notice estimate the rewards for the specified user
      * @param user the user to estimate rewards for
      */
     function estimateConfigUserRewards(
@@ -292,43 +338,26 @@ contract SimpleLockedStaking is Ownable, ReentrancyGuard {
     }
 
     /**
-     * estimates the reward tokens that will be obtained for
-     * the given input stake amount
-     *
-     * actual formula:
-     *      rewardsToBe = rewardRate * amount / RE_FACTOR
-     *
-     * @param stakeAmount the staked amount to use in the estimation
-     *
+     * @dev Estimates the rewards based on the stake amount. The formula used is:
+     *      reward = (stakeAmount * rewardRate) / REWARD_FACTOR.
+     * @param stakeAmount The amount of staked tokens to estimate the reward for.
+     * @return expectedReward The estimated reward tokens for the given stake.
      */
     function estimateRewards(
         uint256 stakeAmount
     ) public view returns (uint256 expectedReward) {
-        uint256 mul1 = C.rewardRate * stakeAmount;
-        if (mul1 - (mul1 % REWARD_FACTOR) > 0) {
-            return (mul1 - (mul1 % REWARD_FACTOR)) / REWARD_FACTOR;
-        } else {
-            return 0;
-        }
+        expectedReward = (stakeAmount * C.rewardRate) / REWARD_FACTOR;
     }
 
     /**
-     * estimates the stake tokens required to obtain the input
-     * reward amount
-     *
-     * actual formula:
-     *      maxStake = rewardBalance * RE_FACTOR / rewardRate
-     *
-     * @param rewardAmount the reward amount to use in the estimation
+     * @dev Estimates the max stake based on the reward amount. The formula used is:
+     *      stake = (rewardAmount * REWARD_FACTOR) / rewardRate.
+     * @param rewardAmount The amount of reward tokens to estimate the stake for.
+     * @return maxStake The estimated  max stake tokens for the given reward.
      */
     function estimateStake(
         uint256 rewardAmount
     ) public view returns (uint256 maxStake) {
-        uint256 mul1 = rewardAmount * REWARD_FACTOR;
-        if (mul1 - (mul1 % C.rewardRate) > 0) {
-            return (mul1 - (mul1 % C.rewardRate)) / REWARD_FACTOR;
-        } else {
-            return 0;
-        }
+        maxStake = (rewardAmount * REWARD_FACTOR) / C.rewardRate;
     }
 }
