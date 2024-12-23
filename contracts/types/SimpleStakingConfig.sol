9d0caa92d8a2d0c3d675a22a2c7540b1b542d0f1
Francisco <francisco.munoz@forcast.cl>
2024-12-23 13:34:41 -0300

    🚧 create specific types file for simple staking

---
+++ contracts/types/SimpleStakingConfig.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BSL-1.0
+pragma solidity >=0.8.0 <0.9.0;
+
+struct StakingConfig{
+    uint256 dprOver10kk;
+    uint256 tokensForRewards;
+    uint256 lockPeriodDuration;
+    uint256 depositPeriodDuration;
+    uint256 startDate;
+}
+
+struct StakingConfigUsage {
+    StakingConfig config;
+    mapping(address => uint256) _stakedPerUser;
+    uint256 _activeUsersCounter;
+    uint256 _totalStaked;
+    uint256 _claimedRewards;
+    bool dropped;
+}
+
+enum ConfigState {
+    PreOpened, // 0
+    Opened, // 1
+    Locked, // 2
+    Completed, // 3
+    Dropped, // 4
+    NotSet // 5
+}
