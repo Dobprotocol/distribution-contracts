4f65fc84aacfa11fabee69961deea1a2d53e3418
Francisco <francisco.munoz@forcast.cl>
2025-04-03 20:46:33 -0300

    fix description of transferToken task

--- tasks/tasks_erc20/transferToken.ts
+++ tasks/tasks_erc20/transferToken.ts
@@ -2,7 +2,7 @@ import { subtask, task } from "hardhat/config";
 import { deployerContract, contractAt } from "../utils/contract-utils";
 import { getSigner } from "../utils/simulation-utils";
 
-task("transferToken", "task to deploy the ERC20 Dob Token ")
+task("transferToken", "task to transfer tokens between holder address and toAddress")
     .addPositionalParam("tokenAddress", "the address of the token")
     .addPositionalParam("ownerAddress", "The address of the owner of the token. Must match the private key from .env file")
     .addPositionalParam("toAddress", "the address who will receive the tokens")
