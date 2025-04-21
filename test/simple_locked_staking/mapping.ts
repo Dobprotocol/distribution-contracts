d4a8c9841bb484c7385d9f9f157a91bcc74c7758
Francisco <francisco.munoz@forcast.cl>
2025-04-21 17:15:53 -0400

    📝 save mapping of tests for simple stacking

---
+++ test/simple_locked_staking/mapping.ts
@@ -0,0 +1,40 @@
+/**
+ * mapping of test files where each functionality of the contract should be tested
+
+* rewardtoken
+    * [x] test_deploy 
+* stakeToken
+    * [x] test_deploy
+* constructor(deploy)
+    * [x] test_deploy
+* stake()
+    * [x] test_stake
+* claim()
+    * [x] test_claim
+* withdrawRemains()
+    * [x] test_claim
+* setConfig()
+    * [x] test_deploy
+* getConfig()
+    * [x] test_deploy
+* getConfigUsageData()
+    * [x] test_stake
+    * [x] test_claim
+* getUserStakedAmount()
+    * [x] test_stake
+    * [x] test_claim
+* getTotalLockedRewards()
+    * [x] test_stake
+    * [x] test_claim
+* getState()
+    * [x] test_deploy
+* estimateConfigUserRewards()
+    * [x] test_claim
+* getRewardTokenBalance()
+    * [x] test_deploy
+* estimateRewards()
+    * [x] test_deploy
+* estimateStake()
+    * [x] test_deploy
+ * 
+ */
